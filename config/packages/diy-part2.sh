84a5db1582aa655769433fb2e123976214a92da2
liuran001 <32791471+liuran001@users.noreply.github.com>
2020-08-30 01:36:43 +0800

    Update diy-part2.sh

--- config/packages/diy-part2.sh
+++ config/packages/diy-part2.sh
@@ -17,10 +17,6 @@ git clone https://github.com/jerrykuku/luci-app-vssr
 git clone https://github.com/Lienol/openwrt-package
 git clone https://github.com/tty228/luci-app-serverchan
 git clone https://github.com/cnzd/luci-app-koolproxyR
-cd lean
-rm -rf luci-theme-argon  
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git 
-cd ..
 mkdir -p luci-app-diskman && \
 mkdir -p parted && \
 wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Makefile -O luci-app-diskman/Makefile
