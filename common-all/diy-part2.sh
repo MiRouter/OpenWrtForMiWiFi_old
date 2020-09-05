4ff1241d3312e77012b4bde076a49e34c52c6a2d
liuran001 <liuran001@126.com>
2020-09-05 15:51:57 +0800

    ababab

--- common-all/diy-part2.sh
+++ common-all/diy-part2.sh
@@ -13,4 +13,4 @@
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 cd package
-git clone https://github.com/fw876/helloworld
+git clone -b packages-19.07 https://github.com/liuran001/openwrt-packages
