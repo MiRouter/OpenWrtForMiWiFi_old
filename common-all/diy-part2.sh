c08f7af4f6e16b7ba91e481d7259883fdb92e484
liuran001 <32791471+liuran001@users.noreply.github.com>
2020-09-06 16:15:45 +0800

    Update diy-part2.sh

--- common-all/diy-part2.sh
+++ common-all/diy-part2.sh
@@ -12,6 +12,8 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+rm -rf ./tools
+svn co https://github.com/coolsnowwolf/lede/trunk/tools tools
 cd package
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/mt
 git clone https://github.com/liuran001/openwrt-packages
